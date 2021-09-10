51cc59fa2cfbcefd06f38696648657bc2a5a4159
hieubui09 <hieubdph09616@fpt.edu.vn>
2021-09-11 00:39:44 +0700

    updata

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(document).ready(function(){
+   $('#show-btn').click(function(){
+
+    $('.click-here').toggleClass('active');
+    $('.light').toggleClass('on');
+   })
+})
+
